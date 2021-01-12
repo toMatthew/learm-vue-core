e501cebaf5858330be53d98f362850d1ba35a668
Matthew <Matthew9298@outlook.com>
2021-01-12 11:41:25 +0800

    Create dep.js

---
+++ observer/dep.js
@@ -0,0 +1,39 @@
+// 订阅和通知的核心
+var uid = 0;
+
+/**
+ * A dep is an observable that can have multiple
+ * directives subscribing to it.
+ */
+var Dep = function Dep () {
+  this.id = uid++; // 用于Watcher来判断是否要新增观察等作用 => this.newDepIds.add(id);  this.newDeps.push(dep);
+  this.subs = [];
+};
+
+Dep.prototype.addSub = function addSub (sub) {
+  this.subs.push(sub);
+};
+
+Dep.prototype.removeSub = function removeSub (sub) {
+  remove(this.subs, sub);
+};
+
+Dep.prototype.depend = function depend () {
+  if (Dep.target) {
+    Dep.target.addDep(this);
+  }
+};
+
+Dep.prototype.notify = function notify () {
+  // stabilize the subscriber list first
+  var subs = this.subs.slice();
+  if (!config.async) { // 如果是异步的话就必须按顺序执行，保证正确的触发
+    // subs aren't sorted in scheduler if not running async
+    // we need to sort them now to make sure they fire in correct
+    // order
+    subs.sort(function (a, b) { return a.id - b.id; });
+  }
+  for (var i = 0, l = subs.length; i < l; i++) {
+    subs[i].update();
+  }
+};
